c6cee526268a6f9d89bbb891dbc0eafa606beaf8
KanJJ <2669511192@qq.com>
2018-01-12 01:06:47 +0800

    update xinbaba

---
+++ xinbaba_core/src/main/java/com/hsbc/App.java
@@ -0,0 +1,13 @@
+package com.hsbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
